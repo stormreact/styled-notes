50d23d54694f159a9796cf4862d502e9960a7c5d
Michael Angerman <stormasm@gmail.com>
2017-07-26 11:38:14 -0700

    starting to work with multiple banners

---
+++ docs2/banner1.js
@@ -0,0 +1,10 @@
+const banner = `<Flex p={3}
+  style={{ height: '100%' }}
+  align='center'>
+  <Box width={[ 1, '640px' ]}>
+  <p>Hola</p>
+  </Box>
+</Flex>
+`
+
+export default banner
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 const path = require('path')
 
 module.exports = {
-  entry: './docs1/entry.js',
+  entry: './docs2/entry.js',
 
   output: {
     filename: 'bundle.js',
-    path: path.join(__dirname, 'docs1')
+    path: path.join(__dirname, 'docs2')
   },
 
   module: {
@@ -19,7 +19,7 @@ module.exports = {
   },
 
   devServer: {
-    contentBase: 'docs1/',
+    contentBase: 'docs2/',
     historyApiFallback: true
   }
 }
